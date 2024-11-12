c7ecd37da3366bc9dccf2cc8c85ec2859de5ed38
Волошко Марина <95192268+marla124@users.noreply.github.com>
2024-11-12 13:27:02 +0300

    controllers

---
+++ EventsWebApplication/EventsWebApplication/Controllers/Participant/Get/GetEventParticipantsController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using EventsWebApplication.Application.UseCases.ParticipantUseCases.Interface;
+using EventsWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventsWebApplication.Controllers.Participant.Get
+{
+    [Route("api/Participant")]
+    [ApiController]
+    public class GetEventParticipantsController(IGetEventParticipantsUseCase getEventParticipantsUseCase, 
+        IMapper mapper) : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetEventParticipants(Guid eventId, CancellationToken cancellationToken)
+        {
+            var practicants = await getEventParticipantsUseCase.Execute(eventId, cancellationToken);
+            return Ok(mapper.Map<List<UserViewModel>>(practicants));
+        }
+
+    }
+}
